89605ec6a5c8a265ecda42c4b4e608adefcf074d
Renzo <renzol2@illinois.edu>
2022-01-14 23:56:51 -0600

    Add tests

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,5 +1,54 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        other.width <= self.width && other.height <= self.height
+    }
+}
+
+fn add_two(x: i32) -> i32 {
+    x + 2
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 7,
+            height: 2,
+        };
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 7,
+            height: 2,
+        };
+        assert!(!smaller.can_hold(&larger));
+    }
+
+    #[test]
+    fn add_two_adds_correctly() {
+        let x = 1;
+        assert_eq!(add_two(x), 3);
+    }
+
     #[test]
     fn exploration() {
         assert_eq!(2 + 2, 4);
