442968a458fe90066ab75a22099375687678c812
Renzo <renzol2@illinois.edu>
2022-01-14 23:47:31 -0600

    Write first tests

---
+++ adder/src/lib.rs
@@ -0,0 +1,13 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn exploration() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    #[should_panic]
+    fn another() {
+        panic!("at the disco");
+    }
+}
