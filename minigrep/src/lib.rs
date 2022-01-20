b47d0e049599ac2f1737739a23d3315a503c7d94
Renzo <renzol2@illinois.edu>
2022-01-20 10:39:01 -0600

    Refactor code into lib

---
+++ minigrep/src/lib.rs
@@ -0,0 +1,29 @@
+use std::error::Error;
+use std::fs;
+
+pub struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments")
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    println!("Searching for {}", config.query);
+    println!("in file {}", config.filename);
+
+    let contents = fs::read_to_string(config.filename)?;
+    println!("With text: {}", contents);
+
+    Ok(())
+}
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,23 +1,7 @@
 use std::env;
-use std::fs;
 use std::process;
 
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments")
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
-    }
-}
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -27,10 +11,8 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("in file {}", config.filename);
-
-    let contents =
-        fs::read_to_string(config.filename).expect("Something went wrong when reading the file");
-    println!("With text: {}", contents);
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
