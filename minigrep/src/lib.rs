7ce0180fb7155e87bf972e418161aac7348a0999
Renzo <renzol2@illinois.edu>
2022-05-22 14:58:13 -0500

    Rewrite library

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,29 +1,26 @@
 use std::error::Error;
 use std::fs;
 
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+    println!("With contents: {}", contents);
+
+    Ok(())
+}
+
 pub struct Config {
-    query: String,
-    filename: String,
+    pub query: String,
+    pub filename: String,
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &str> {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            return Err("Not enough arguments")
+            return Err("not enough arguments");
         }
         let query = args[1].clone();
         let filename = args[2].clone();
 
         Ok(Config { query, filename })
     }
-}
-
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    println!("Searching for {}", config.query);
-    println!("in file {}", config.filename);
-
-    let contents = fs::read_to_string(config.filename)?;
-    println!("With text: {}", contents);
-
-    Ok(())
 }
