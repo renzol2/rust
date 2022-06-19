6f5339223ec66f98d3f2a1caf81c3d755d65cf03
Renzo <renzol2@illinois.edu>
2022-06-19 18:30:18 -0500

    Separate UI & engine code and add tests

---
+++ amazing_adventures/src/engine.rs
@@ -0,0 +1,210 @@
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+#[derive(Clone, Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+/// A `Direction` represents a direction a Player can take.
+pub struct Direction {
+    direction_name: String,
+    destination: String,
+}
+
+#[derive(Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+/// A `RoomData` struct contains room information parsed from JSON.
+pub struct RoomData {
+    pub name: String,
+    pub description: String,
+    pub items: Option<Vec<String>>,
+    pub directions: Vec<Direction>,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+/// `MapData` contains map information parsed from JSON.
+pub struct MapData {
+    starting_room: String,
+    ending_room: String,
+    rooms: Vec<RoomData>,
+}
+
+impl MapData {
+    pub fn get_adventure_map(&self) -> Result<AdventureMap, String> {
+        // Check if map has starting room
+        match self
+            .rooms
+            .iter()
+            .find(|room| room.name == self.starting_room)
+        {
+            Some(_) => {}
+            None => return Err(String::from("Map does not contain starting room in rooms")),
+        };
+
+        // Check if map has ending room room
+        match self.rooms.iter().find(|room| room.name == self.ending_room) {
+            Some(_) => {}
+            None => return Err(String::from("Map does not contain ending room in rooms")),
+        };
+
+        // Construct room map from rooms vector
+        let mut room_name_map = HashMap::new();
+        for room in &self.rooms {
+            let directions_map: HashMap<String, Direction> = room
+                .directions
+                .iter()
+                .map(|dir| (dir.direction_name.clone(), dir.clone()))
+                .collect();
+
+            let adventure_room = AdventureRoom {
+                name: room.name.clone(),
+                description: room.description.clone(),
+                items: room.items.as_ref().unwrap_or(Vec::new().as_ref()).clone(),
+                directions_map,
+            };
+            room_name_map.insert(room.name.clone(), adventure_room);
+        }
+
+        // Check if every direction leads to a room
+        for room in &self.rooms {
+            for direction in &room.directions {
+                if !room_name_map.contains_key(&direction.destination) {
+                    let error_msg = format!(
+                        "Room {} contains a direction {} to room {}, which does not exist.",
+                        room.name, direction.direction_name, direction.destination
+                    );
+                    return Err(error_msg);
+                }
+            }
+        }
+
+        let am = AdventureMap {
+            starting_room: self.starting_room.clone(),
+            ending_room: self.ending_room.clone(),
+            room_name_map,
+        };
+
+        Ok(am)
+    }
+}
+
+/// An `AdventureRoom` represents a room that a Player can enter and exit.
+pub struct AdventureRoom {
+    pub name: String,
+    pub description: String,
+    pub items: Vec<String>,
+    pub directions_map: HashMap<String, Direction>,
+}
+
+/// `AdventureMap` represents the entire map for an adventure game.
+pub struct AdventureMap {
+    pub starting_room: String,
+    pub ending_room: String,
+    pub room_name_map: HashMap<String, AdventureRoom>,
+}
+
+/// `Player` holds the player's state during an adventure game.
+pub struct Player {
+    items: Vec<String>,
+}
+
+impl Player {
+    pub fn new(initial_items: Vec<String>) -> Player {
+        Player {
+            items: initial_items,
+        }
+    }
+}
+
+#[derive(PartialEq, Debug)]
+pub enum Command {
+    Go { direction: String },
+    Take { item: String },
+    Drop { item: String },
+    Quit,
+    Invalid,
+}
+
+/// `AdventureEngine` exposes the interface for creating and running an adventure game.
+pub struct AdventureEngine {
+    pub map: AdventureMap,
+    pub player: Player,
+    pub current_room: String,
+}
+
+impl AdventureEngine {
+    /// Constructs a new `AdventureEngine` struct from given map data.
+    /// Returns an error if map data is not valid.
+    pub fn new(map_data: MapData) -> Result<AdventureEngine, String> {
+        let adventure_map = match map_data.get_adventure_map() {
+            Ok(map) => map,
+            Err(error_msg) => return Err(error_msg),
+        };
+
+        Ok(AdventureEngine {
+            current_room: adventure_map.starting_room.clone(),
+            map: adventure_map,
+            player: Player::new(Vec::new()),
+        })
+    }
+
+    /// Updates game state based on player command. Returns true
+    /// if command was received successfully, or false otherwise.
+    pub fn process_command(&mut self, command: Command) -> AdventureState {
+        match command {
+            Command::Go { direction } => {
+                let current_room = self
+                    .map
+                    .room_name_map
+                    .get(&self.current_room)
+                    .unwrap_or_else(|| panic!("Error: current room not found in rooms map"));
+
+                let destination_room = current_room.directions_map.get(&direction);
+
+                match destination_room {
+                    Some(direction) => {
+                        self.current_room = direction.destination.clone();
+                        if direction.destination == self.map.ending_room {
+                            AdventureState::Finish
+                        } else {
+                            AdventureState::Success
+                        }
+                    }
+                    None => AdventureState::Failure {
+                        error_msg: "You can't go that direction from this room.".to_string(),
+                    },
+                }
+            }
+            Command::Quit => AdventureState::Quit,
+            _ => AdventureState::Failure {
+                error_msg: "That command is not recognized.".to_string(),
+            },
+        }
+    }
+}
+
+pub enum AdventureState {
+    Success,
+    Failure { error_msg: String },
+    Quit,
+    Finish,
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs;
+
+    use super::*;
+
+    #[test]
+    fn get_adventure_map_creates_map_correctly() {
+        let file = fs::read_to_string("resources/dorm.json").unwrap();
+        let map: MapData = serde_json::from_str(&file).unwrap();
+
+        let adventure_map = map.get_adventure_map().unwrap();
+
+        assert_eq!(adventure_map.starting_room, "DormRoom");
+        assert_eq!(adventure_map.ending_room, "LaundryRoom");
+
+        // TODO: ensure some rooms have correct names, descriptions, items, and directions
+    }
+}
--- amazing_adventures/src/lib.rs
+++ amazing_adventures/src/lib.rs
@@ -1,209 +1,245 @@
-use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
+use clap::Parser;
+use std::fs;
+use std::io;
+use std::process;
 
-#[derive(Clone, Serialize, Deserialize, Debug)]
-#[serde(rename_all = "camelCase")]
-/// A `Direction` represents a direction a Player can take.
-pub struct Direction {
-    direction_name: String,
-    destination: String,
+mod engine;
+use engine::*;
+
+#[derive(Parser, Debug)]
+pub struct Args {
+    /// The path to the file to read
+    #[clap(parse(from_os_str))]
+    path: std::path::PathBuf,
 }
 
-#[derive(Clone, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-/// A `RoomData` struct contains room information parsed from JSON.
-pub struct RoomData {
-    pub name: String,
-    pub description: String,
-    pub items: Option<Vec<String>>,
-    pub directions: Vec<Direction>,
-}
+pub fn run_game(args: Args) {
+    let file = fs::read_to_string(args.path).unwrap_or_else(|err| {
+        eprintln!(
+            "An error occurred while locating JSON file: {}",
+            err.to_string()
+        );
+        process::exit(1);
+    });
+    let map: MapData = serde_json::from_str(&file).unwrap_or_else(|err| {
+        eprintln!(
+            "An error occurred while deserializing JSON file: {}",
+            err.to_string()
+        );
+        process::exit(1);
+    });
 
-/// An `AdventureRoom` represents a room that a Player can enter and exit.
-pub struct AdventureRoom {
-    pub name: String,
-    pub description: String,
-    pub items: Vec<String>,
-    pub directions_map: HashMap<String, Direction>,
-}
-
-#[derive(Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-/// `MapData` contains map information parsed from JSON.
-pub struct MapData {
-    starting_room: String,
-    ending_room: String,
-    rooms: Vec<RoomData>,
-}
-
-impl MapData {
-    pub fn get_adventure_map(&self) -> Result<AdventureMap, String> {
-        // Check if map has starting room
-        match self
-            .rooms
-            .iter()
-            .find(|room| room.name == self.starting_room)
-        {
-            Some(_) => {}
-            None => return Err(String::from("Map does not contain starting room in rooms")),
-        };
-
-        // Check if map has ending room room
-        match self.rooms.iter().find(|room| room.name == self.ending_room) {
-            Some(_) => {}
-            None => return Err(String::from("Map does not contain ending room in rooms")),
-        };
-
-        // Construct room map from rooms vector
-        let mut room_name_map = HashMap::new();
-        for room in &self.rooms {
-            let directions_map: HashMap<String, Direction> = room
-                .directions
-                .iter()
-                .map(|dir| (dir.direction_name.clone(), dir.clone()))
-                .collect();
-
-            let adventure_room = AdventureRoom {
-                name: room.name.clone(),
-                description: room.description.clone(),
-                items: room.items.as_ref().unwrap_or(Vec::new().as_ref()).clone(),
-                directions_map,
-            };
-            room_name_map.insert(room.name.clone(), adventure_room);
+    let mut adventure_engine: AdventureEngine = match AdventureEngine::new(map) {
+        Ok(engine) => engine,
+        Err(msg) => {
+            eprintln!("An error occurred while initializing adventure: {}", msg);
+            process::exit(1);
         }
+    };
 
-        // Check if every direction leads to a room
-        for room in &self.rooms {
-            for direction in &room.directions {
-                if !room_name_map.contains_key(&direction.destination) {
-                    let error_msg = format!(
-                        "Room {} contains a direction {} to room {}, which does not exist.",
-                        room.name, direction.direction_name, direction.destination
-                    );
-                    return Err(error_msg);
-                }
-            }
-        }
+    // Game loop begins
+    let final_state: AdventureState = initiate_game_loop(&mut adventure_engine);
 
-        let am = AdventureMap {
-            starting_room: self.starting_room.clone(),
-            ending_room: self.ending_room.clone(),
-            room_name_map,
-        };
-
-        Ok(am)
-    }
-}
-
-/// `AdventureMap` represents the entire map for an adventure game.
-pub struct AdventureMap {
-    pub starting_room: String,
-    pub ending_room: String,
-    pub room_name_map: HashMap<String, AdventureRoom>,
-}
-
-/// `Player` holds the player's state during an adventure game.
-pub struct Player {
-    items: Vec<String>,
-}
-
-impl Player {
-    pub fn new(initial_items: Vec<String>) -> Player {
-        Player {
-            items: initial_items,
-        }
-    }
-}
-
-pub enum Command {
-    Go { direction: String },
-    Take { item: String },
-    Drop { item: String },
-    Quit,
-    Invalid,
-}
-
-/// `AdventureEngine` exposes the interface for creating and running an adventure game.
-pub struct AdventureEngine {
-    pub map: AdventureMap,
-    pub player: Player,
-    pub current_room: String,
-}
-
-impl AdventureEngine {
-    /// Constructs a new `AdventureEngine` struct from given map data.
-    /// Returns an error if map data is not valid.
-    pub fn new(map_data: MapData) -> Result<AdventureEngine, String> {
-        let adventure_map = match map_data.get_adventure_map() {
-            Ok(map) => map,
-            Err(error_msg) => return Err(error_msg),
-        };
-
-        Ok(AdventureEngine {
-            current_room: adventure_map.starting_room.clone(),
-            map: adventure_map,
-            player: Player::new(Vec::new()),
-        })
-    }
-
-    /// Updates game state based on player command. Returns true
-    /// if command was received successfully, or false otherwise.
-    pub fn process_command(&mut self, command: Command) -> AdventureState {
-        match command {
-            Command::Go { direction } => {
-                let current_room = self
+    match final_state {
+        AdventureState::Finish => {
+            println!(
+                "{}",
+                adventure_engine
                     .map
                     .room_name_map
-                    .get(&self.current_room)
-                    .unwrap_or_else(|| panic!("Error: current room not found in rooms map"));
-
-                let destination_room = current_room.directions_map.get(&direction);
-
-                match destination_room {
-                    Some(direction) => {
-                        self.current_room = direction.destination.clone();
-                        if direction.destination == self.map.ending_room {
-                            AdventureState::Finish
-                        } else {
-                            AdventureState::Success
-                        }
-                    }
-                    None => AdventureState::Failure {
-                        error_msg: "You can't go that direction from this room.".to_string(),
-                    },
-                }
-            }
-            Command::Quit => AdventureState::Quit,
-            _ => AdventureState::Failure {
-                error_msg: "That command is not recognized.".to_string(),
-            },
+                    .get(&adventure_engine.current_room)
+                    .unwrap()
+                    .description
+            );
+            println!("You won!");
         }
+        AdventureState::Quit => {
+            println!("You quit.");
+        }
+        _ => (),
     }
 }
 
-pub enum AdventureState {
-    Success,
-    Failure { error_msg: String },
-    Quit,
-    Finish,
+pub fn initiate_game_loop(engine: &mut AdventureEngine) -> AdventureState {
+    let final_state = loop {
+        let current_room = engine.map.room_name_map.get(&engine.current_room).unwrap();
+        println!("\n{}", current_room.description);
+        println!("You can go in these directions: ");
+        for direction in &current_room.directions_map {
+            println!("- {}", direction.0);
+        }
+        let items = &current_room.items;
+        if !items.is_empty() {
+            println!("You see these items on the ground: ");
+            for item in items {
+                println!("- {}", item);
+            }
+        }
+        println!("\nWhat would you like to do?\n");
+
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        let command = process_input(input, false);
+
+        let state = engine.process_command(command);
+
+        match state {
+            AdventureState::Success => (),
+            AdventureState::Failure { error_msg } => println!("{}", error_msg),
+            AdventureState::Finish => break AdventureState::Finish,
+            AdventureState::Quit => break AdventureState::Quit,
+        }
+        println!("");
+    };
+
+    final_state
+}
+
+/// Converts raw user input into commands.
+/// Trims whitespace and return characters.
+pub fn process_input(input: String, is_case_sensitive: bool) -> Command {
+    // TODO: add support for items
+    // Separate commands from directions/items
+    let input = input.trim();
+    let tokens: Vec<&str> = input.splitn(2, " ").collect();
+    let command = tokens[0];
+    let command: String = if is_case_sensitive {
+        command.to_string()
+    } else {
+        command.to_lowercase()
+    };
+
+    // Identify command
+    if command == "go" {
+        let direction = tokens[1].to_string();
+        let direction = direction.trim().to_string();
+        return Command::Go { direction };
+    } else if command == "quit" {
+        return Command::Quit;
+    } else {
+        return Command::Invalid;
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::fs;
-
     use super::*;
 
     #[test]
-    fn get_adventure_map_creates_map_correctly() {
-        let file = fs::read_to_string("resources/dorm.json").unwrap();
-        let map: MapData = serde_json::from_str(&file).unwrap();
-        
-        let adventure_map = map.get_adventure_map().unwrap();
+    fn process_command_go_single_word_lowercase() {
+        let input = String::from("go north");
+        let processed_input = process_input(input, true);
+        let expected = Command::Go {
+            direction: "north".to_string(),
+        };
+        assert_eq!(processed_input, expected);
+    }
 
-        assert_eq!(adventure_map.starting_room, "DormRoom");
-        assert_eq!(adventure_map.ending_room, "LaundryRoom");
+    #[test]
+    fn process_command_go_single_word_uppercase() {
+        let input = String::from("go SOUTH");
+        let processed_input = process_input(input, true);
+        let expected = Command::Go {
+            direction: "SOUTH".to_string(),
+        };
+        assert_eq!(processed_input, expected);
+    }
 
-        // TODO: ensure some rooms have correct names, descriptions, items, and directions
+    #[test]
+    fn process_command_go_single_word_mixed_case() {
+        let input = String::from("go wEsT");
+        let processed_input = process_input(input, true);
+        let expected = Command::Go {
+            direction: "wEsT".to_string(),
+        };
+        assert_eq!(processed_input, expected);
+    }
+
+    #[test]
+    fn process_command_go_single_word_lowercase_spaced() {
+        let input = String::from("    go     up    ");
+        let processed_input = process_input(input, true);
+        let expected = Command::Go {
+            direction: "up".to_string(),
+        };
+        assert_eq!(processed_input, expected);
+    }
+
+    #[test]
+    fn process_command_go_single_word_uppercase_spaced() {
+        let input = String::from("    go     UP    ");
+        let processed_input = process_input(input, true);
+        let expected = Command::Go {
+            direction: "UP".to_string(),
+        };
+        assert_eq!(processed_input, expected);
+    }
+
+    #[test]
+    fn process_command_go_single_word_mixed_case_spaced() {
+        let input = String::from("    go           down the spooky hallway    ");
+        let processed_input = process_input(input, true);
+        let expected = Command::Go {
+            direction: "down the spooky hallway".to_string(),
+        };
+        assert_eq!(processed_input, expected);
+    }
+
+    #[test]
+    fn process_command_go_multi_word_lowercase() {
+        let input = String::from("go down the hallway");
+        let processed_input = process_input(input, true);
+        let expected = Command::Go {
+            direction: "down the hallway".to_string(),
+        };
+        assert_eq!(processed_input, expected);
+    }
+
+    #[test]
+    fn process_command_go_multi_word_uppercase() {
+        let input = String::from("go FAR, FAR AWAY");
+        let processed_input = process_input(input, true);
+        let expected = Command::Go {
+            direction: "FAR, FAR AWAY".to_string(),
+        };
+        assert_eq!(processed_input, expected);
+    }
+
+    #[test]
+    fn process_command_go_multi_word_mixed_case() {
+        let input = String::from("go behind the Holy Cathedral");
+        let processed_input = process_input(input, true);
+        let expected = Command::Go {
+            direction: "behind the Holy Cathedral".to_string(),
+        };
+        assert_eq!(processed_input, expected);
+    }
+
+    #[test]
+    fn process_command_quit_case_sensitive_valid() {
+        let input = String::from("quit");
+        let processed_input = process_input(input, true);
+        let expected = Command::Quit;
+        assert_eq!(processed_input, expected);
+    }
+
+    #[test]
+    fn process_command_quit_case_sensitive_invalid() {
+        let input = String::from("quIT");
+        let processed_input = process_input(input, true);
+        let expected = Command::Invalid;
+        assert_eq!(processed_input, expected);
+    }
+
+    #[test]
+    fn process_command_quit_case_insensitive_valid() {
+        let input = String::from("quIT");
+        let processed_input = process_input(input, false);
+        let expected = Command::Quit;
+        assert_eq!(processed_input, expected);
     }
 }
