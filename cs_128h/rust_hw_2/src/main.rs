499e7ce46d297ba6ae6eeb895ef99d1910dc2f29
Renzo <renzol2@illinois.edu>
2022-12-20 17:36:09 -0600

    Complete HW2

--- cs_128h/rust_hw_2/src/main.rs
+++ cs_128h/rust_hw_2/src/main.rs
@@ -1,7 +1,13 @@
 extern crate hw2;
 
 fn main() {
-
-    println!("School Colors! {:#?} and {:#?}", hw2::color_string_to_enum(Some(String::from("blue"))).unwrap(), hw2::color_string_to_enum(Some(String::from("orange"))).unwrap());
-    println!("Fibbonaci numbers not exceeding: {:?}", hw2::fibonacci_odd_sum(1111111111));
+    println!(
+        "School Colors! {:#?} and {:#?}",
+        hw2::color_string_to_enum(Some(String::from("blue"))).unwrap(),
+        hw2::color_string_to_enum(Some(String::from("orange"))).unwrap()
+    );
+    println!(
+        "Fibbonaci numbers not exceeding: {:?}",
+        hw2::fibonacci_odd_sum(1111111111)
+    );
 }
