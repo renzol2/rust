f1a2ee49644561b3c4575445200029cfbd54b535
Renzo <renzol2@illinois.edu>
2022-12-20 18:58:05 -0600

    Finish HW3

--- cs_128h/rust_hw_3/src/lib.rs
+++ cs_128h/rust_hw_3/src/lib.rs
@@ -3,7 +3,17 @@
 /// divide by 4, subtract 4, then subtract 1 if the number is odd, and multiply by 9
 /// return the result
 pub fn math_formula_one(mut target: i32) -> i32 {
-    todo!();
+    target *= 6;
+    target += match target % 2 {
+        0 => 8,
+        _ => 9,
+    };
+    target /= 4;
+    target -= 4;
+    if target % 2 != 0 {
+        target -= 1;
+    }
+    return target * 9;
 }
 
 /// TODO: Implement this function that passes a parameter by reference
@@ -11,24 +21,35 @@ pub fn math_formula_one(mut target: i32) -> i32 {
 /// Subtract 10 from the input until the input is less than 200
 /// Multiply the input by 15   
 pub fn math_formula_two(target: &mut i32) {
-    todo!();
+    *target = (*target / 2) * 3 - 3;
+    *target /= 10;
+    while *target >= 200 {
+        *target -= 10;
+    }
+    *target *= 15;
 }
 
-/// TODO: Create a function called "runner" with no input that returns an i32. 
+/// TODO: Create a function called "runner" with no input that returns an i32.
 /// Create a variable holding the year
 /// Call math_formula_one and pass the variable as a parameter
-/// Is the value of the variable holding the year still the same?
+/// Is the value of the variable holding the year still the same?  
+///     Yes... I think
 /// Call math_formula_two and pass the variable as a parameter
 /// Is the value of the variable holding the year still the same now? Did anything change? Why?
 /// Return the value returned by the function minus the variable holding the year
-  
+pub fn runner() -> i32 {
+    let mut year = 2022;
+    let value = math_formula_one(year);
+    math_formula_two(&mut year) ;
+    value - year
+}
 
 // You can test your code with the test cases we've provided by running `cargo test`
 // We also encourage you to add more assert statements to test out your code further!
 #[cfg(test)]
 mod test {
     use super::*;
-    
+
     #[test]
     fn test_math_formula_one() {
         assert_eq!(math_formula_one(10), 108);
@@ -44,7 +65,6 @@ mod test {
 
     #[test]
     fn test_runner() {
-        // [Your test here...]
+        assert_eq!(runner(), 24375);
     }
-
 }
