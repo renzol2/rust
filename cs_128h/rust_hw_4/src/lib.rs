f3b7c8f11a28cecfa2c94cd8199fa615ecd99f39
Renzo <renzol2@illinois.edu>
2022-12-20 20:03:35 -0600

    Implement vowel counting

--- cs_128h/rust_hw_4/src/lib.rs
+++ cs_128h/rust_hw_4/src/lib.rs
@@ -1,8 +1,8 @@
 /// TODO: Implement this function that passes a mutable Vector of Strings
-/// You must iterate through the vector. 
+/// You must iterate through the vector.
 /// If the word has an even number of characters, reverse the word by using the reverse function.
 /// If the word has an odd number of characters, count the number of vowels in the word.
-/// You should return a tuple containg a Vector of the reversed words 
+/// You should return a tuple containg a Vector of the reversed words
 /// and the total number of vowels from odd lengthed words.
 pub fn vector_iter(words: &mut Vec<String>) -> (Vec<String>, i32) {
     todo!();
@@ -20,22 +20,69 @@ pub fn reverse_word(word: &mut String) {
 /// You should not modify the contents of the passed String.
 /// You should return the vowel count.
 pub fn count_vowels(word: &str) -> i32 {
-    todo!();
+    let mut vowel_count = 0;
+    for c in word.to_lowercase().chars() {
+        match c {
+            'a' | 'e' | 'i' | 'o' | 'u' => vowel_count += 1,
+            _ => {}
+        }
+    }
+    vowel_count
 }
-
 
 // You can test your code with the test cases we've provided by running `cargo test`
 // We also encourage you to add more assert statements to test out your code further!
 #[cfg(test)]
 mod test {
     use super::*;
-    
+
     #[test]
     fn test_vector_iter() {
-        let words: &mut Vec<String> = &mut vec!["fish".to_string(), "cat".to_string(), "ardvaark".to_string(), "dog".to_string(), "bird".to_string(), "ant".to_string()];
+        let words: &mut Vec<String> = &mut vec![
+            "fish".to_string(),
+            "cat".to_string(),
+            "ardvaark".to_string(),
+            "dog".to_string(),
+            "bird".to_string(),
+            "ant".to_string(),
+        ];
         let (reversed_words, count) = vector_iter(words);
-        assert_eq!(reversed_words, vec!["hsif".to_string(), "kraavdra".to_string() ,"drib".to_string()]);
+        assert_eq!(
+            reversed_words,
+            vec![
+                "hsif".to_string(),
+                "kraavdra".to_string(),
+                "drib".to_string()
+            ]
+        );
         assert_eq!(count, 3);
     }
 
+    #[test]
+    fn test_reverse_word() {
+        let mut s = "music".to_string();
+        reverse_word(&mut s);
+        println!("{}", s);
+    }
+
+    #[test]
+    fn test_count_vowels() {
+        let mut s = "music";
+        let vowel_count = count_vowels(&mut s);
+        assert_eq!(vowel_count, 2);
+    }
+
+    #[test]
+    fn test_count_vowels_all_vowels() {
+        let mut s = "aeiou";
+        let vowel_count = count_vowels(&mut s);
+        assert_eq!(vowel_count, 5);
+    }
+
+    #[test]
+    fn test_count_vowels_no_vowels() {
+        let mut s = "cdpsrwz";
+        let vowel_count = count_vowels(&mut s);
+        assert_eq!(vowel_count, 0);
+    }
 }
